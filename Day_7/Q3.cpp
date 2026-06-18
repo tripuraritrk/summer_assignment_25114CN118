fe13c1c1c06163a04468dc310833b32ab36b49c2
Tripurari Kumar <tripuraritrk@gmail.com>
2026-06-18 07:37:53 +0530

    Day_7 assignment

---
+++ Day_7/Q3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int SumOfDigits(int n)
+{
+    if(n==0)
+        return 0;
+
+        return (n%10)+SumOfDigits(n/10);
+
+}
+int main() {
+    
+    int n;
+
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    cout<<"Sum of digits = " <<SumOfDigits(n);
+    
+    return 0;
+}
