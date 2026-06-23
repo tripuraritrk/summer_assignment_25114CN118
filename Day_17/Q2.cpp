55d32f55c29fdcc5966bc632e432ea865250bf25
Tripurari Kumar <tripuraritrk@gmail.com>
2026-06-23 17:19:40 +0530

    Day_17 assignment

---
+++ Day_17/Q2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+
+    cout<<"Enter size of first array: ";
+    cin>>n1;
+
+    int arr1[n1];
+
+    cout<<"Enter elements of first array: ";
+    for(int i=0; i<n1; i++)
+    {
+        cin>>arr1[i];
+    }
+    cout<<"Enter size of second array: ";
+    cin>>n2;
+
+    int arr2[n2];
+
+    cout<<"Enter elements of second array: ";
+    for(int i=0; i<n2; i++)
+    {
+        cin>>arr2[i];
+    }
+
+    cout<<"Union of array: ";
+
+    //Print first array
+    for(int i=0; i<n2;i++)
+    {
+
+        cout<<arr1[i]<<" ";
+    }
+
+    //print only unique elements from second array
+    for(int i=0; i<n2; i++)
+    {
+        bool found = false;
+
+        for(int j =0; j<n1; j++)
+        {
+            if(arr2[i]==arr1[j])
+            {
+                found=true;
+                break;
+            }
+        }
+        if(!found)
+        cout<<arr2[i]<<" ";
+    }
+
+    return 0;
+}
